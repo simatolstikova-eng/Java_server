578c07a96b71163389cc227babfe983bd2663cfa
Sima Tolstikova <simatolstikova@gmail.com>
2026-05-06 11:13:52 +0300

    Add JsonUtil for JSON conversion

---
+++ src/main/java/com/example/util/JsonUtil.java
@@ -0,0 +1,24 @@
+package com.example.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+public class JsonUtil {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String toJson(Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
